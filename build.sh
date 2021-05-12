853f04e89a34d451df47e35750cfdf5ea444ccbe
wuyushuwys <wuyushuwys@outlook.com>
2021-05-12 16:22:49 -0400

    install virtuanenv via apt

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ fi
 # Install pip3 and virtualenv
 echo "Install pip3 and virtualenv"
 sudo apt install python3-pip
-sudo python3 -m pip install virtualenv
-
+# sudo python3 -m pip install virtualenv
+sudo apt-get install python3-venv
 
 # Install jupyterhub
 echo "Install jupyterhub"
